faadbc3092b6db5413210a33fc5ba7f0536297cb
Stefan Müller <stefan.mueller@ucd.ie>
2025-06-22 21:13:36 +0100

    Add 2024 names

--- tests/data_creation/create_iebabynames.R
+++ tests/data_creation/create_iebabynames.R
@@ -3,13 +3,39 @@ library(stringr)
 library(dplyr)
 library(usethis)
 
-dat <- readtext("/Users/smueller/Dropbox/datasets/babynames_ireland/*")
+dat <- readtext("babynames_ireland/*")
 
-dat_22 <- readtext("/Users/smueller/Dropbox/datasets/babynames_2022/*")
+dat_22 <- readtext("babynames_2022/*")
 
-dat_23 <- readtext("/Users/smueller/Dropbox/datasets/babynames_2023/*") |>
+dat_23 <- readtext("babynames_2023/*") |>
     filter(Year == "2023") |>
     filter(!str_detect(`Statistic.Label`, "Rank")) # remove rank observations
+
+dat_24 <- readtext("babynames_2024/*") |>
+   filter(Year == "2024") |>
+   filter(!str_detect(`Statistic.Label`, "Rank")) # remove rank observations
+
+
+iebabynames_24 <- dat_24 |>
+   mutate(sex = ifelse(str_detect(`Statistic.Label`, "Boys"), "Male",
+                       ifelse(str_detect(`Statistic.Label`, "Girls"), "Female", NA))) %>%
+   mutate(name = ifelse(is.na(`Boys.Names`), `Girls.Names`, `Boys.Names`)) |> 
+   rename(year = Year,
+          n = VALUE) |>
+   filter(n != "") |> # remove names that appeared fewer than three times
+   mutate(n = as.integer(n)) |>
+   group_by(sex) |>
+   arrange(sex, -n) |>
+   group_by(year, sex) |>
+   mutate(rank = base::rank(-n, ties.method = "min")) |>
+   ungroup() |>
+   mutate(prop = n / sum(n)) %>%
+   group_by(year, sex) %>%
+   mutate(prop_sex = n / sum(n)) %>%
+   ungroup() %>%
+   dplyr::select(year, sex, name, n, rank, prop, prop_sex) |>
+   as.data.frame()
+
 
 
 iebabynames_23 <- dat_23 |>
@@ -31,6 +57,7 @@ iebabynames_23 <- dat_23 |>
     ungroup() %>%
     dplyr::select(year, sex, name, n, rank, prop, prop_sex) |>
     as.data.frame()
+
 
 
 iebabynames_22 <- dat_22 |>
@@ -73,6 +100,6 @@ iebabynames_pre22 <- dat %>%
     ungroup() %>%
     as.data.frame()
 
-iebabynames <- bind_rows(iebabynames_23, iebabynames_22, iebabynames_pre22)
+iebabynames <- bind_rows(iebabynames_24, iebabynames_23, iebabynames_22, iebabynames_pre22)
 
 usethis::use_data(iebabynames, overwrite = TRUE)
